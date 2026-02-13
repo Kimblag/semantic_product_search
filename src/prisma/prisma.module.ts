9b4a180ae591c3b7bd8ba6e0e071acafed21a747
Kimblag <kimberly.blandong@gmail.com>
2026-02-13 11:09:33 -0300

    feat(database): setup prisma with sqlite schema and migrations

---
+++ src/prisma/prisma.module.ts
@@ -0,0 +1,10 @@
+import { Global, Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+
+// make it global so we don't have to import it in every module
+@Global()
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService],
+})
+export class PrismaModule {}
