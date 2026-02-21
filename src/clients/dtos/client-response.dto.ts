313ff9e3030f101da23f4559003e60598ec8445c
Kimblag <kimberly.blandong@gmail.com>
2026-02-21 15:18:11 -0300

    refactor(dtos): Remove unnecesary 'Expose' decorator from dtos.

--- src/clients/dtos/client-response.dto.ts
+++ src/clients/dtos/client-response.dto.ts
@@ -1,18 +1,9 @@
-import { Expose } from 'class-transformer';
-
 export class ClientResponseDto {
-  @Expose()
   id: string;
-  @Expose()
   name: string;
-  @Expose()
   email: string;
-  @Expose()
   telephone: string;
-  @Expose()
   address: string;
-  @Expose()
   active: boolean;
-  @Expose()
   createdAt: Date;
 }
--- src/providers/dtos/provider-response.dto.ts
+++ src/providers/dtos/provider-response.dto.ts
@@ -1,27 +1,10 @@
-import { Expose } from 'class-transformer';
-
 export class ProviderResponseDto {
-  @Expose()
   id: string;
-
-  @Expose()
   code: string;
-
-  @Expose()
   name: string;
-
-  @Expose()
   email: string;
-
-  @Expose()
   telephone: string;
-
-  @Expose()
   address: string;
-
-  @Expose()
   active: boolean;
-
-  @Expose()
   createdAt: Date;
 }
